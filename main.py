db4a3906735baa34bd593f9f66df321e0e846751
Isaac Fonseca Vasconcelos <20231101110007@ifrn.local>
2026-05-27 08:09:50 -0300

    iniciando pagina login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -7,34 +7,39 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+
 @app.get("/", response_class=HTMLResponse)
 def home():
-
     with open("templates/index.html", "r", encoding="utf-8") as arquivo:
-        html = arquivo.read()
+        return HTMLResponse(content=arquivo.read())
 
-    return HTMLResponse(content=html)
+
+
+@app.get("/login-page", response_class=HTMLResponse)
+def login_page():
+    with open("templates/login.html", "r", encoding="utf-8") as arquivo:
+        return HTMLResponse(content=arquivo.read())
+
+
+
+@app.post("/login")
+async def login(request: Request):
+    data = await request.json()
+
+    usuario = data.get("usuario")
+    senha = data.get("senha")
+
+    if usuario == "admin" and senha == "123":
+        return {"mensagem": "Login realizado com sucesso"}
+    
+    return {"mensagem": "Usuário ou senha inválidos"}
 
 
 @app.post("/register")
 def register():
-
-    return {
-        "mensagem": "Usuário cadastrado"
-    }
-
-
-@app.post("/login")
-def login():
-
-    return {
-        "mensagem": "Login realizado"
-    }
+    return {"mensagem": "Usuário cadastrado"}
 
 
 @app.get("/produtos")
 def produtos():
-
-    return {
-        "produtos": []
-    }
+    return {"produtos": []}
